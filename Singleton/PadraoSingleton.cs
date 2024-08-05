eb243d5d3bd1dca70ac5631e1121c12045947708
Felipe eu mesmo <luiz_sena@hotmail.com>
2024-08-05 00:28:06 -0300

    add: singleton

---
+++ Singleton/PadraoSingleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton {
+    public class PadraoSingleton {
+        private static PadraoSingleton instance = null;
+        private static object lockObjeto = new object();
+        private static int contador = 0;
+
+        public PadraoSingleton()
+        {
+            contador++;
+            Console.WriteLine($"Instância: {contador}");
+        }
+
+        public static PadraoSingleton Instance() {
+            if(instance == null) {
+                lock (lockObjeto) {
+                    if(instance == null) 
+                        instance = new PadraoSingleton();
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton {
+    internal class Program {
+        static void Main(string[] args) {
+            Parallel.Invoke(
+                () => AcessoThread1(),
+                () => AcessoThread2()
+                );
+            Console.ReadLine();
+        }
+
+        private static void AcessoThread1() {
+            PadraoSingleton s1 = PadraoSingleton.Instance();
+            Console.WriteLine("Thread 1");
+        }
+
+        private static void AcessoThread2() {
+            PadraoSingleton s1 = PadraoSingleton.Instance();
+            Console.WriteLine("Thread 2");
+        }
+    }
+}
