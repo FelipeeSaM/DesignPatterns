477dc683eebccfce79f25e40efdae7cff23f5098
Felipe eu mesmo <luiz_sena@hotmail.com>
2024-08-11 01:29:03 -0300

    add: padrão adapter

---
+++ Adapter/JSONDataProcessor.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter {
+    public class JSONDataProcessor : IDataProcessor {
+        private JObject _jsonData;
+
+        public JSONDataProcessor() {
+            _jsonData = new JObject();
+        }
+
+        public string ReadData() {
+            return _jsonData.ToString();
+        }
+
+        public void SaveData(string data) {
+            _jsonData = JObject.Parse(data);
+        }
+    }
+}
---
+++ Adapter/XMLDataProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Adapter {
+    public class XMLDataProcessor {
+        private XElement _xmlData;
+
+        public XMLDataProcessor() {
+            _xmlData = new XElement("Root");
+        }
+
+        public string ReadXML() {
+            return _xmlData.ToString();
+        }
+
+        public void SaveXML(string data) {
+            _xmlData = XElement.Parse(data);
+        }
+    }
+}
